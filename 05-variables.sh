0661062bb97010cb53d0847e971aa91dd7e2e0a5
vanitha540 <rvanitha.babu@gmail.com>
2024-08-26 14:50:47 +0800

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,12 @@
 
 echo "please enter your username::"
 
-read USERNAME
+read -s USERNAME
 
 echo "username entered is: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
+echo "password entered is: $PASSWORD"
