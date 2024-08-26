423977b7800e30a89c8843838267489789f0b056
vanitha540 <rvanitha.babu@gmail.com>
2024-08-26 22:22:16 +0800

    functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -2,7 +2,15 @@
 
 USERID=$(id -u)
 
-echo "User ID is: $USERID"
+CHECK_ROOT(){
+    
+if [ $USERID -ne 0 ]
+ then 
+    echo "please run this script with root priveleges"
+    exit 1
+ fi
+
+}
 
 VALIDATE(){
 if [  $1 -ne 0]
@@ -14,14 +22,9 @@ if [  $1 -ne 0]
 
      fi
 }
-       
- if [ $USERID -ne 0 ]
- then 
-    echo "please run this script with root priveleges"
-    exit 1
- fi
+ CHECK_ROOT
 
-dnf list installed git
+  dnf list installed git
 
 if [ $? -ne 0 ]
 then
