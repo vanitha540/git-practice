28a6deb6152c9f512e6b91910be028336c8d1994
vanitha540 <rvanitha.babu@gmail.com>
2024-08-26 20:51:06 +0800

    functions

--- 11-installation.sh
+++ 11-installation.sh
@@ -10,5 +10,17 @@ echo "User ID is: $USERID"
     exit 1
  fi
 
-dnf install git -y
+dnf list installed git
+
+if [ $? -ne 0]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+else
+    echo "Git is already installed, nothing to do it.."
+
+ fi   
+        
+
+
 
