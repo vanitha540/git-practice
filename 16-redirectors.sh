3d2b233186918780087e3faeb18fbf7c1f5b631c
vanitha540 <rvanitha.babu@gmail.com>
2024-08-28 00:13:24 +0800

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -24,10 +24,10 @@ VALIDATE(){
 if [  $1 -ne 0 ]
 
  then
-     echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE | tee -a $LOG_FILE
+     echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
      exit 1
  else 
-     echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE | tee -a $LOG_FILE
+     echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
 
      fi
 }
